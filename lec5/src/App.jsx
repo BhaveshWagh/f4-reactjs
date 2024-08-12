105e28202c64a6854c150701f5bd1597136d8675
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-12 21:38:41 +0530

    setup

---
+++ lec5/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+    <>
+      <p>12 Aug 2024</p>
+      <h1>Lecture 05th </h1>
+    </>
+  );
+}
+
+export default App;
